b44fe1f23c61df126ad6c9a316cd5f64c80ab60f
이길상 <oat641@gmail.com>
2021-04-09 16:22:54 +0900

    dev_cube update

--- Logic/dart console/dart_test/lib/dev_cube.dart
+++ Logic/dart console/dart_test/lib/dev_cube.dart
@@ -11,28 +11,56 @@ class Cube {
 
   /// 전개도 유형. 6개 면의 행-열 좌표 표기
   ///
-  ///  전개도 디폴트(type 0)
-  ///                ┌────┐
-  ///                │ 00 │
-  /// ┌────┬────┬────┼────┤
-  /// │ 01 │ 02 │ 03 │ 04 │
-  /// ├────┼────┴────┴────┘
-  /// │ 05 │
+  ///  전개도 디폴트(type 0)            ________
+  ///                ┌────┐           /       /\
+  ///                │ 00 │          /  0↑   /  \
+  /// ┌────┬────┬────┼────┤         /_______/ ↘ \
+  /// │ 01 │ 02 │ 03 │ 04 │         \       \ 2  /
+  /// ├────┼────┴────┴────┘          \  1←   \  /
+  /// │ 05 │                          \_______\/
   /// └────┘
   ///
-  ///
-  ///
-  /*3D 전개도 유형을 나타낼 수 있는 데이터 구조 고안
-  - 정육면체의 각면에 0~5의 번호를 지정했다고 가정
-  - 11가지 전개도 유형에서 각 면의 위치를 행과 열이 표시된 문자열로 표기
-  - 동시에 각 면의 회전 상태를 0~3의 번호로 표기 
-  - 전개도를 생성할 때, 무작위로  유형을 선택
-  */
-  ///
-  var type;
-  var direction;
-  List<dynamic> order = [1, 2, 3, 4, 5, 6];
+  var example;
+  var suggestion;
+  var answer;
 
+  /// 등각투영도와 방향 : 각각 0,1,2 순서대로 표기. 화살표방향이 0, 시계 반대방향 회전
+  static var isometrics = [
+    [
+      [0, 1, 2],
+      [3, 3, 2]
+    ],
+    [
+      [0, 2, 3],
+      [2, 3, 2]
+    ],
+    [
+      [0, 3, 4],
+      [1, 3, 2]
+    ],
+    [
+      [0, 4, 1],
+      [0, 3, 2]
+    ],
+    [
+      [1, 5, 2],
+      [0, 3, 1]
+    ],
+    [
+      [2, 5, 3],
+      [0, 0, 1]
+    ],
+    [
+      [3, 5, 4],
+      [0, 1, 1]
+    ],
+    [
+      [4, 5, 1],
+      [0, 2, 1]
+    ]
+  ];
+
+  /// 전개도 유형별 좌표
   static var types = [
     ['03', '10', '11', '12', '13', '20'],
     ['03', '10', '11', '12', '13', '21'],
@@ -47,6 +75,7 @@ class Cube {
     ['02', '20', '11', '12', '03', '21'],
   ];
 
+  /// 전개도 유형별 면 방향 0~3 : 시계 반대 방향 회전
   static var directions = [
     ['0', '0', '0', '0', '0', '0'],
     ['0', '0', '0', '0', '0', '1'],
@@ -61,11 +90,19 @@ class Cube {
     ['1', '1', '0', '0', '1', '1'],
   ];
 
-  /// duplicate 가 true로 주어지면 중복을 허용하여 뽑음. 기본값은 false
-  static List rand(int value, int range, [bool duplicate = false]) {
-    var list = new List();
-    var rng = new Random();
-    for (var i = 0; i < value; i++) {
+  @override
+  String toString() {
+    var ex = '예시 : $example \n';
+    var su = '보기: $suggestion \n';
+    var an = '정답: $answer \n';
+    return ex + su + an;
+  }
+
+  /// [0, range) 범위의 number개의 수. duplicate 가 true로 주어지면 중복을 허용하여 선택. 기본값은 false
+  static List rand(int number, int range, [bool duplicate = false]) {
+    var list = [];
+    var rng = Random();
+    for (var i = 0; i < number; i++) {
       int n;
       do {
         n = rng.nextInt(range);
@@ -84,12 +121,29 @@ class Cube {
     return false;
   }
 
+  /// 등각투영도가 예시로, 여러 전개도가 보기로 주어진다
   /// mode가 true이면 정답 하나를 찾기.
   /// mode가 false이면 다른 하나를 찾기.
-  static List devList(int count, bool mode) {
-    var cubes = [];
-    var list = rand(count, types.length);
-    var orders = [];
+  void isoQuestion(int count, bool mode) {
+    var turn = rand(1, 2)[0];
+    var iso = isometrics[rand(1, 8)[0]];
+    for (var i = 0; i < turn; i++) {
+      var a = iso[0][0], b = iso[1][0];
+      iso[0][0] = iso[0][1];
+      iso[0][1] = iso[0][2];
+      iso[0][2] = a;
+      iso[1][0] = iso[1][1];
+      iso[1][1] = iso[1][2];
+      iso[1][2] = b;
+    }
+    example = iso;
+
+    answer = rand(1, count)[0];
+
+    suggestion = [];
+    var list = rand(count, types.length); //전개도 유형 번호
+    var orders = []; //면의 순서
+
     var max = (mode) ? count : 2;
 
     for (var i = 0; i < max; i++) {
@@ -100,21 +154,49 @@ class Cube {
       orders.add(order);
     }
 
-    cubes.add(Cube(list[0], orders[0]));
-    for (var i = 1; i < count; i++) {
-      cubes.add(Cube(list[i], orders[(mode) ? i : 1]));
+    for (var i = 0; i < count; i++) {
+      var n = list[i]; // 유형
+      suggestion.add([
+        types[n],
+        directions[n],
+        orders[(mode) ? i : ((i == answer) ? 0 : 1)]
+      ]);
+    }
+  }
+
+  /// 전개도가 예시로, 여러 등각투영도가 보기로 주어진다
+  /// mode가 true이면 정답 하나를 찾기.
+  /// mode가 false이면 다른 하나를 찾기.
+  void devQuestion(int count, bool mode) {
+    example = isometrics[rand(1, 8)[0]];
+
+    answer = rand(1, count)[0];
+
+    suggestion = [];
+    var list = rand(count, types.length); //전개도 유형 번호
+    var orders = []; //면의 순서
+
+    var max = (mode) ? count : 2;
+
+    for (var i = 0; i < max; i++) {
+      var order;
+      do {
+        order = rand(6, 6);
+      } while (isIn(orders, order));
+      orders.add(order);
     }
 
-    return cubes;
+    for (var i = 0; i < count; i++) {
+      var n = list[i]; // 유형
+      suggestion.add([
+        types[n],
+        directions[n],
+        orders[(mode) ? i : ((i == answer) ? 0 : 1)]
+      ]);
+    }
   }
 
-  Cube([int n = 0, this.order]) {
-    this.type = types[n];
-    this.direction = directions[n];
-  }
-
-  @override
-  String toString() {
-    return "($type,$direction,$order)\n";
+  Cube() {
+    isoQuestion(1, true);
   }
 }
