119e6f5f3e82de9559fcd5a13730b5a135db4626
이길상 <oat641@gmail.com>
2021-04-11 17:38:08 +0900

    level 0 equality check

--- Logic/dart console/dart_test/bin/dart_test.dart
+++ Logic/dart console/dart_test/bin/dart_test.dart
@@ -1,11 +1,12 @@
+import 'dart:convert';
 import 'dart:math';
 import 'package:collection/collection.dart';
 
 import 'package:dart_test/dev_cube.dart';
 
 void main(List<String> arguments) {
-  for (var i = 0; i < 5; i++) {
-    for (var j = 0; j < 3; j++) {
+  for (var i = 0; i < 4; i++) {
+    for (var j = 0; j < 1; j++) {
       var cube = DevCube(j, i);
       print('$cube');
     }
